ce205e99347814040172b35c2ffb3539470550da
Deepak R <deepak.shankar@knolskape.com>
2017-06-13 21:54:02 +0530

    ch1

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('I love roshu');
+  
+  res.send(req.body);
 })
 
 app.listen(process.env.PORT || 3000, function () {
